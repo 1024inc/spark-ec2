1c80370d392bb987420246adaf9ca5cbb92b5da7
Jason Rubenstein <jasondrubenstein@gmail.com>
2015-08-04 16:25:06 -0700

    move the swap file management to the proper script

--- create-swap.sh
+++ create-swap.sh
@@ -10,10 +10,22 @@ if [ -e /mnt/swap ]; then
   exit 1
 fi
 
-SWAP_MB=$1
-if [[ "$SWAP_MB" != "0" ]]; then
-  dd if=/dev/zero of=/mnt/swap bs=1M count=$SWAP_MB
-  mkswap /mnt/swap
-  swapon /mnt/swap
-  echo "Added $SWAP_MB MB swap file /mnt/swap"
-fi
+# This scipt tries to put a 1gb swap partition on an 8gb drive. Once
+# spark is built, that leaves us with a few hundred meg free. If the /mnt/swap
+# file already exists, it doesn't try, so we just touch the file and deal with
+# swap ourselves.
+touch /mnt/swap
+
+SWAP_MB=1024
+
+dd if=/dev/zero of=/vol0/swap bs=1M count=$SWAP_MB
+dd if=/dev/zero of=/vol1/swap bs=1M count=$SWAP_MB
+
+mkswap /vol0/swap
+mkswap /vol1/swap
+
+chmod 600 /vol0/swap
+chmod 600 /vol1/swap
+
+swapon /vol0/swap
+swapon /vol1/swap
