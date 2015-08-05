6b2c92d51c1848cb2316b8fe5eb8cc399f54f15d
Jason Rubenstein <jasondrubenstein@gmail.com>
2015-08-05 09:28:06 -0700

    change java reference in templates from 1.7 to 1.8

--- templates/root/tachyon/conf/tachyon-env.sh
+++ templates/root/tachyon/conf/tachyon-env.sh
@@ -20,7 +20,7 @@ if [[ `uname -a` == Darwin* ]]; then
 else
   # Assuming Linux
   if [ -z "$JAVA_HOME" ]; then
-    export JAVA_HOME=/usr/lib/jvm/java-1.7.0
+    export JAVA_HOME=/usr/lib/jvm/java-1.8.0
   fi
   export TACHYON_RAM_FOLDER=/mnt/ramdisk
 fi
