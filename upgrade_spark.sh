648a466c02eac576ec063a43287e0cc1cfb575d3
Jason Rubenstein <jasondrubenstein@gmail.com>
2015-09-28 14:20:55 -0700

    script to upgrade spark via a commit hash

---
+++ upgrade_spark.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+DEFAULT_SPARK_GITHUB_REPO="https://github.com/apache/spark"
+
+if [[ $# -eq 0 ]]; then
+	echo "Spark git commit hash required: usage upgrade_spark.sh <HASH>"
+	exit 1
+fi
+
+if [[ -z "$1" ]]; then
+	echo "Spark git hash must not be blank."
+	exit 1
+fi
+
+/root/spark/sbin/stop-all.sh
+/root/persistent-hdfs/sbin/stop-all.sh
+
+# Make sure we are in the spark-ec2 directory
+pushd /root/spark-ec2 > /dev/null
+
+#export the git hash for spark
+export SPARK_VERSION="$DEFAULT_SPARK_GITHUB_REPO|$1"
+export HADOOP_MAJOR_VERSION="YARN"
+
+#Upgrade Spark
+if [[ -e spark/init.sh ]]; then
+    source spark/init.sh upgrade
+	cd /root/spark-ec2  # guard against init.sh changing the cwd
+fi
+
+if [[ -e spark/setup.sh ]]; then
+    source spark/setup.sh
+	cd /root/spark-ec2  # guard against setup.sh changing the cwd
+fi
+
+popd > /dev/null
+
+exit 0
