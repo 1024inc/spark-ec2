d0330134f763c03bf55780e16f38595ed8de1fd0
Jason Rubenstein <jasondrubenstein@gmail.com>
2015-09-28 17:18:41 -0700

    No, I was right the first time. Put the job control in the upgrade script.

--- spark/setup.sh
+++ spark/setup.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
 
 /root/spark-ec2/copy-dir /root/spark
-
-/root/persistent-hdfs/sbin/start-all.sh && /root/persistent-hdfs/sbin/restart-nfs.sh
-/root/spark/sbin/start-all.sh
-
--- upgrade_spark.sh
+++ upgrade_spark.sh
@@ -35,6 +35,11 @@ if [[ -e spark/setup.sh ]]; then
 	cd /root/spark-ec2  # guard against setup.sh changing the cwd
 fi
 
+#start the services
+/root/persistent-hdfs/sbin/start-all.sh && /root/persistent-hdfs/sbin/restart-nfs.sh
+/root/spark/sbin/start-all.sh
+
+
 popd > /dev/null
 
 exit 0
